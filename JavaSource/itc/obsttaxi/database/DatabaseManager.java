1c978954c585be4cb83f4fd50ac67539aa1cc3f5
Nikita Kubasov <nikita.kubasovv@gmail.com>
2020-05-27 15:25:10 +0200

    Datenbank  angepasst

--- JavaSource/itc/obsttaxi/database/DatabaseManager.java
+++ JavaSource/itc/obsttaxi/database/DatabaseManager.java
@@ -7,6 +7,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DatabaseManager {
     
@@ -69,18 +71,59 @@ public class DatabaseManager {
         return obst;
     }
 
+    public void AddKunde(String vollerName, String adresse, int postleitzahl){
+        try (PreparedStatement ps = connection
+                .prepareStatement("INSERT INTO kunden ('name', 'adresse') VALUES (?, ?);")) {
+
+            ps.setString(1, vollerName);
+            ps.setString(2, adresse + " " + postleitzahl);
+            ps.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void AddBestellung(int kundenId){
+        try (PreparedStatement ps = connection
+                .prepareStatement("INSERT INTO bestellungen ('datum', 'kundenid') VALUES (?, ?);")) {
+
+            ps.setInt(2, kundenId);
+            ps.setDate(1, Date.valueOf(LocalDate.now()));
+            ps.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void AddBestellPosition(int kundenId, int obstid, int menge){
+        try (PreparedStatement ps = connection
+                .prepareStatement("INSERT INTO bestell_positionen ('menge', 'bestellungsid', 'kundenid') VALUES (?, ?, ?);")) {
+
+            ps.setInt(1, menge);
+            ps.setInt(2, kundenId);
+            ps.setInt(3, kundenId);
+
+            ps.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void handleDB() {
         try {
             Statement stmt = connection.createStatement();
             
             stmt.executeUpdate("CREATE TABLE IF NOT EXISTS obst (name varchar(30), preis decimal(4, 2), id integer primary key);");
             
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS kunden (vorname varchar(20), name varchar(20), adresse varchar(20), kundenid integer primary key);");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS kunden (name varchar(20), adresse varchar(30), kundenid integer primary key);");
             
             stmt.executeUpdate("CREATE TABLE IF NOT EXISTS bestellungen (datum varchar(20), kundenid integer, id integer primary key,  FOREIGN KEY(kundenid) REFERENCES kunden(kundenid));");
 
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS bestell_positionen (menge integer, bestellungsid integer, kundenid integer, id integer primary key,  "
-            		+ "FOREIGN KEY(kundenid) REFERENCES kunden(kundenid), FOREIGN KEY(bestellungsid) REFERENCES bestellungen(id));");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS bestell_positionen (menge integer, bestellungsid integer, obstid integer, id integer primary key,  "
+            		+ "FOREIGN KEY(kundenid) REFERENCES obst(obstid), FOREIGN KEY(bestellungsid) REFERENCES bestellungen(id));");
 
             PreparedStatement ps = connection
                     .prepareStatement("INSERT INTO obst VALUES (?, ?, ?);");
